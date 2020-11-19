d9ec17e8aebe616ea3e301a6a29666e0d6588037
Tomasz Dudziec <tomasz.dudziec@redembedded.com>
2020-11-19 23:51:27 +0100

    add initial reactor code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React,  { useState} from 'react'
 import './App.css';
+import TodoList from './TodoList'
+
 
 function App() {
+  const[todos,setTodos]= useState(['Taskk1 ','Tasdk2 '])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <TodoList todos={todos} />
+      <input type="text"/>
+      <button>Add Task todo</button>
+      <button>Clear Completed Task</button>
+      <div>0 left to do </div>
+  
+    </>
+       )
 }
 
 export default App;
+  
